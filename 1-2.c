f8e281c2859eaab52d513f0ec2b28db2ca1278c9
Hepheir <hepheir@gmail.com>
2023-09-19 07:48:20 +0000

    Add 1-2.c

---
+++ 1-2.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    if (access("linux. txt", F_OK) == -1)
+    {
+        perror("linux.txt");
+        exit(1);
+    }
+    return 0;
+}
