fb7de8d61510b1b74298823630c7ef77726343ba
Hepheir <hepheir@gmail.com>
2023-09-26 09:19:15 +0000

    Add comment to 1-5.c

--- 1-5.c
+++ 1-5.c
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
     int i, sum = 0;
 
+    /**
+     * 0번 인자에는 자신(프로그램)이 주어지므로 해당 인자를 생략하고,
+     * 사용자가 문자의 형태로 입력한 인자들을 숫자로 파싱하여 누적 합을 구한다.
+     */
     for(i = 1; i < argc; i++)
         sum += atoi(argv[i]);
 
