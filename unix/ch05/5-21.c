b6ba67587543c598162107b941077c80516de064
Hepheir <hepheir@gmail.com>
2023-10-31 07:17:55 +0000

    ctime() 처럼 출력하는 기능 추가

--- unix/ch05/5-21.c
+++ unix/ch05/5-21.c
@@ -5,7 +5,8 @@
 char *output[] = {
     "%x %X",
     "%G년 %m월 %d일 %U주 %H:%M",
-    "%r"
+    "%r",
+    "%a %b %e %T %Y" // ctime() 과 같게.
 };
 
 int main() {
@@ -17,7 +18,7 @@ int main() {
     time(&timep);
     tm = localtime(&timep);
 
-    for (n = 0; n < 3; n++) {
+    for (n = 0; n < 4; n++) {
         strftime(buf, sizeof(buf), output[n], tm);
         printf("%s = %s\n", output[n], buf);
     }
