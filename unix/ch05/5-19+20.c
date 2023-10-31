48267ae1ad0565cac7bb7704b857a4656cd87d4f
Hepheir <hepheir@gmail.com>
2023-10-31 06:46:34 +0000

    5-19와 5-20을 합쳐서 실습

---
+++ unix/ch05/5-19+20.c
@@ -0,0 +1,14 @@
+#include <time.h>
+#include <stdio.h>
+
+int main() {
+    struct tm *tm;
+    time_t timep;
+
+    time(&timep);
+    tm = localtime(&timep);
+
+    printf("Time(sec) : %d\n", (int)timep);
+    printf("Time(date, ctime)\t: %s", ctime(&timep));
+    printf("Time(date, asctime)\t: %s", asctime(tm));
+}
