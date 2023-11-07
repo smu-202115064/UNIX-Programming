41a75ddce559a2d22cf527ea822077add3de4df1
Hepheir <hepheir@gmail.com>
2023-11-07 07:46:06 +0000

    wait() 함수의 동작원리를 더 알아보기 위해 멀티 프로세스 fork() 시도

--- unix/ch07/7-8.c
+++ unix/ch07/7-8.c
@@ -7,19 +7,36 @@
 int main() {
     int status;
     pid_t pid;
+    pid_t cpid;
 
-    switch (pid = fork()) {
+    // 4개의 자식 프로세스 생성
+    for (int i = 0; i < 4; i++) {
+        pid = fork();
+        if (pid <= 0) {
+            sleep(3);
+            exit(2);
+        }
+    }
+
+    printf("--> Parent Process\n");
+    printf("Pid to wait for is %d.\n", pid);
+
+    switch (pid) {
         case -1 : /* fork failed */
             perror("fork");
             exit(1);
             break;
         case 0 : /* child process */
             printf("--> Child Process\n");
+            sleep(3);
             exit(2);
             break;
         default : /* parent process */
-            while (wait(&status) != pid)
+            while ((cpid = wait(&status)) != pid) {
+                printf("Parent still wait for %d...\n", cpid);
+                sleep(1);
                 continue;
+            }
             printf("--> Parent process\n");
             printf("Status: %d, %x\n", status, status);
             printf("Child process Exit Status:%d\n", status >> 8); // 1 Byte right-shift
