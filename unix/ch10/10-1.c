8385a288aec41228a0a22f8232bbbe9ed2b2affb
Hepheir <hepheir@gmail.com>
2023-11-21 06:49:54 +0000

    refactor(ch10/10-1.c): popen()에서 얻어진 파일 포인터의 file descriptor 관측을 통해 pipe() sys call 이 내부적으로 있음을 유추해 볼 수 있다.
    r 모드로 읽으면 fd = 3
    w 모드로 읽으면 fd = 4

--- unix/ch10/10-1.c
+++ unix/ch10/10-1.c
@@ -4,12 +4,15 @@
 int main( ) {
     FILE *fp;
     int a;
+    int fd;
 
     fp = popen("wc -l", "w");
     if (fp == NULL) {
         fprintf(stderr, "popen failed\n");
         exit(1);
     }
+    fd = fileno(fp);
+    printf("FD: %d\n", fd);
 
     for (a = 0; a < 100; a++)
         fprintf(fp, "test line\n");
