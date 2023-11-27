1b62727ba8cb5c4330dbadd78cbccd087ec1a798
Hepheir <hepheir@gmail.com>
2023-11-27 06:51:32 +0000

    docs(ch11/11-2.c): 주석 추가

--- unix/ch11/11-3.c
+++ unix/ch11/11-3.c
@@ -7,7 +7,7 @@ int main() {
     int msgid;
 
     key = ftok("keyfile", 1);
-    msgid = msgget(key, IPC_CREAT|0644);
+    msgid = msgget(key, IPC_CREAT|0644); // 큐가 없었어도 여기서 만들어졌을 것.
     if (msgid == -1) {
         perror("msgget");
         exit(1);
@@ -15,7 +15,7 @@ int main() {
 
     printf("Before IPC_RMID\n");
     system("ipcs -q");
-    msgctl(msgid, IPC_RMID, (struct msqid_ds *)NULL);
+    msgctl(msgid, IPC_RMID, (struct msqid_ds *)NULL); // NULL을 주는 이유는 해당 파라미터를 사용하지 않겠다는 의미.
     printf("After IPC_RMID\n");
     system("ipcs -q");
 }
