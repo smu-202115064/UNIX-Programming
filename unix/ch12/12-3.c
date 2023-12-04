1c6bb388e62d903df293049af1fd488ba29be728
Hepheir <hepheir@gmail.com>
2023-12-04 06:51:18 +0000

    feat(ch12/12-3.c): NBO와 HBO모두 출력

--- unix/ch12/12-3.c
+++ unix/ch12/12-3.c
@@ -5,11 +5,26 @@ int main() {
     struct servent *port;
     int n;
 
+
+    printf("Host Byte Order:\n");
+
     setservent(0);
 
     for (n = 0; n < 5; n++) {
         port = getservent();
         printf("Name=%s, Port=%d\n", port->s_name, ntohs(port->s_port));
+    }
+
+    endservent();
+
+
+    printf("Network Byte Order:\n");
+
+    setservent(0);
+
+    for (n = 0; n < 5; n++) {
+        port = getservent();
+        printf("Name=%s, Port=%d\n", port->s_name, port->s_port);
     }
 
     endservent();
