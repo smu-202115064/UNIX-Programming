50c7d3f196b0f3942ce4fef9130a008129f9c613
Hepheir <hepheir@gmail.com>
2023-12-05 06:16:19 +0000

    feat(ch12/12-4.c): HBO를 NBO로 변환하기 (빅,리틀 엔디안 비교)

--- unix/ch12/12-4.c
+++ unix/ch12/12-4.c
@@ -5,8 +5,10 @@ int main() {
     struct servent *port;
 
     port = getservbyname("ssh", "tcp");
-    printf("Name=%s, Port=%d\n", port->s_name, ntohs(port->s_port));
+    printf("[NBO] Name=%s, Port=%d\n", port->s_name, port->s_port);
+    printf("[HBO] Name=%s, Port=%d\n", port->s_name, ntohs(port->s_port));
 
     port = getservbyport(htons(21), "tcp");
-    printf("Name=%s, Port=%d\n", port->s_name, ntohs(port->s_port));
+    printf("[NBO] Name=%s, Port=%d\n", port->s_name, port->s_port);
+    printf("[HBO] Name=%s, Port=%d\n", port->s_name, ntohs(port->s_port));
 }
